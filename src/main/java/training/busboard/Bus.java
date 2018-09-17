a3e2022c06dce4258259bf966a3a88f8cbe0d06b
Jess <jessica.gilbert@softwire.com>
2018-09-17 12:09:00 +0100

    Part 1: created bus objects

---
+++ src/main/java/training/busboard/Bus.java
@@ -0,0 +1,17 @@
+package training.busboard;
+
+public class Bus {
+
+    public int timeToStation;
+    public String lineName;
+
+
+    public int getTime(){
+        return timeToStation;
+    }
+
+    public String getBusName(){
+        return lineName;
+    }
+
+}
--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,15 +1,27 @@
 package training.busboard;
 
+//import everything we need//
 import org.glassfish.jersey.jackson.JacksonFeature;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
 
 public class Main {
     public static void main(String args[]) {
 
+        //creates builder for json reader//
         Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
-        client.
 
+        //creates a list of object bus, gets Json file from URL//
+        List<Bus> busList = client
+                .target("https://api-argon.tfl.gov.uk")
+                .path("StopPoint/490008660N/Arrivals")
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .get(new GenericType<List<Bus>>() {});
     }
+
+
 }
