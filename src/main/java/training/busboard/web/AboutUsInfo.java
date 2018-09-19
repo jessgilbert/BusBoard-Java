f8b12145cf2d3c8a9a120d79f44e5b71234e4eef
Jess <jessica.gilbert@softwire.com>
2018-09-19 16:54:23 +0100

    About us info page
    Staff with pictures

---
+++ src/main/java/training/busboard/web/AboutUsInfo.java
@@ -0,0 +1,13 @@
+package training.busboard.web;
+
+import java.time.LocalDate;
+
+public class AboutUsInfo {
+
+public String FirstName;
+public String LastName;
+public LocalDate DateOfBirth;
+public String FavFood;
+public String FavFilm;
+
+}
--- src/main/java/training/busboard/web/Website.java
+++ src/main/java/training/busboard/web/Website.java
@@ -16,17 +16,36 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @EnableAutoConfiguration
 public class Website {
 
+//    //Creates a list for about us info
+//    List<AboutUsInfo> aboutUsInfoList = new ArrayList<>();
+//
+//    //Reads this file
+//    String filename = "C:\\Users\\JJG\\Work\\BusBoard1\\BusBoardAboutUsTable.csv";
+//    List<String> lines = readTheFile(filename);
+//
+//
+//    aboutus = CreateInfo(lines);
+
+
     // when it is just "/" it tells it to go to index.html//
     @RequestMapping("/")
     ModelAndView home() {
         HomeModel homeModel = new HomeModel();
-        return new ModelAndView("index","homeModel",homeModel);
+        return new ModelAndView("index", "homeModel", homeModel);
     }
 
     @RequestMapping("/aboutus")
@@ -45,17 +64,18 @@ public class Website {
         Coordinates postcodeInfo = null;
 
         //creates Coordinates and CoordinatesResult object, gets Long and Lat//
-         try {
-             postcodeInfo = client
-                     .target("https://api.postcodes.io/postcodes/" + postcode)
-                     .request(MediaType.APPLICATION_JSON_TYPE)
-                     .get(new GenericType<Coordinates>() {});
+        try {
+            postcodeInfo = client
+                    .target("https://api.postcodes.io/postcodes/" + postcode)
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get(new GenericType<Coordinates>() {
+                    });
 
-         } catch (Exception e) {
-             HomeModel homeModel = new HomeModel();
-             homeModel.error = "Invalid Postcode!";
-             return new ModelAndView("index","homeModel", homeModel);
-         }
+        } catch (Exception e) {
+            HomeModel homeModel = new HomeModel();
+            homeModel.error = "Invalid Postcode!";
+            return new ModelAndView("index", "homeModel", homeModel);
+        }
 
 
         //gets the busStop id from long and lat input as an Object "BusStopID"//
@@ -63,7 +83,8 @@ public class Website {
                 .target("https://api-argon.tfl.gov.uk/StopPoint?stopTypes=NaptanPublicBusCoachTram&radius=1000&lat="
                         + postcodeInfo.result.latitude + "&lon=" + postcodeInfo.result.longitude)
                 .request(MediaType.APPLICATION_JSON_TYPE)
-                .get(new GenericType<BusStopID>() {});
+                .get(new GenericType<BusStopID>() {
+                });
 
         //creates an array for busStop object//
         ArrayList<BusStop> busStops = new ArrayList<>();
@@ -101,7 +122,7 @@ public class Website {
 
         // Tells where your sending in ("info")//
         // Class you can access (model) and how to reference it in the html ("model")
-        return new ModelAndView("info", "model", model) ;
+        return new ModelAndView("info", "model", model);
     }
 
     //entry point to the app//
@@ -109,4 +130,35 @@ public class Website {
         SpringApplication.run(Website.class, args);
     }
 
+    public static List<String> readTheFile(String filename) {
+        Path path = Paths.get(filename);
+        List<String> lines = null;
+        try {
+            lines = Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+//    public static List<AboutUsInfo> CreateInfo(List<String> lines) {
+//        ArrayList<AboutUsInfo> as = new ArrayList<AboutUsInfo>();
+//
+//        for (int i = 1; i < lines.size(); i++) {
+//            String line = lines.get(i);
+//
+//            String[] bits = line.split(",");
+//
+//            AboutUsInfo a = new AboutUsInfo();
+//            a.FirstName = bits[0];
+//            a.LastName = bits[1];
+//            a.DateOfBirth = LocalDate.parse(bits[2], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+//            a.FavFilm = bits[3];
+//            a.FavFood = bits[4];
+//            as.add(a);
+//
+//        }
+//
+//        return as;
+//    }
 }
